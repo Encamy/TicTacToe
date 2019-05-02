cce8aff20e1e64c623f2eef56ed2fe4d437eb424
Aleksei <alexxey67@gmail.com>
2019-05-02 20:10:47 +0300

    Added settings

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    //private GameMode m_gamemode = GameMode.GameMode3x3;
+    private GameMode m_gamemode = GameMode.GameMode7x7;
+
+    public Toggle m_3x3Button;
+    public Toggle m_7x7Button;
+
+    public Toggle m_PvPButton;
+    public Toggle m_PvEButton;
+
     void Start()
     {
-
+        switch (m_gamemode)
+        {
+            case GameMode.GameMode3x3:
+                m_3x3Button.isOn = true;
+                break;
+            case GameMode.GameMode7x7:
+                m_7x7Button.isOn = true;
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -25,5 +43,11 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public enum GameMode
+    {
+        GameMode3x3,
+        GameMode7x7,
     }
 }
