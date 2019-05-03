9a66d27a60e6c7c7bd84a1f0838b7b0b8735e4c0
Aleksei <alexxey67@gmail.com>
2019-05-03 16:45:51 +0300

    Cover minimax solver with tests

---
+++ CSharp/SolverTests/MiniMaxSolverTests.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static GameController;
+
+namespace SolverTests
+{
+    [TestClass]
+    public class MiniMaxSolverTests
+    {
+        [TestMethod]
+        public void IsTerminal_Test01()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.None, Player.None, Player.None,
+                Player.None, Player.None, Player.None,
+                Player.None, Player.None, Player.None
+            };
+
+            bool expected = false;
+            Player expectedWinner = Player.None;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test02()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.XPlayer, Player.OPlayer, Player.None,
+                Player.None, Player.XPlayer, Player.None,
+                Player.None, Player.OPlayer, Player.XPlayer
+            };
+
+            bool expected = true;
+            Player expectedWinner = Player.XPlayer;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test03()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.None, Player.None, Player.XPlayer,
+                Player.None, Player.XPlayer, Player.None,
+                Player.XPlayer, Player.None, Player.None
+            };
+
+            bool expected = true;
+            Player expectedWinner = Player.XPlayer;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test04()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.OPlayer, Player.None, Player.OPlayer,
+                Player.None, Player.XPlayer, Player.None,
+                Player.XPlayer, Player.None, Player.None
+            };
+
+            bool expected = false;
+            Player expectedWinner = Player.None;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test05()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.None, Player.None, Player.None,
+                Player.None, Player.XPlayer, Player.OPlayer,
+                Player.None, Player.None, Player.None
+            };
+
+            bool expected = false;
+            Player expectedWinner = Player.None;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test06()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.XPlayer, Player.OPlayer, Player.XPlayer,
+                Player.OPlayer, Player.OPlayer, Player.OPlayer,
+                Player.None, Player.None, Player.None
+            };
+
+            bool expected = true;
+            Player expectedWinner = Player.OPlayer;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test07()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.XPlayer, Player.OPlayer, Player.None,
+                Player.XPlayer, Player.OPlayer, Player.XPlayer,
+                Player.None, Player.OPlayer, Player.XPlayer
+            };
+
+            bool expected = true;
+            Player expectedWinner = Player.OPlayer;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test08()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.None, Player.None, Player.OPlayer,
+                Player.None, Player.XPlayer, Player.None,
+                Player.XPlayer, Player.None, Player.None
+            };
+
+            bool expected = false;
+            Player expectedWinner = Player.None;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test09()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.None, Player.None, Player.XPlayer,
+                Player.None, Player.OPlayer, Player.None,
+                Player.OPlayer, Player.None, Player.None
+            };
+
+            bool expected = false;
+            Player expectedWinner = Player.None;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test10()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.XPlayer, Player.None, Player.None,
+                Player.None, Player.XPlayer, Player.None,
+                Player.None, Player.None, Player.XPlayer
+            };
+
+            bool expected = true;
+            Player expectedWinner = Player.XPlayer;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test11()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.XPlayer, Player.OPlayer, Player.OPlayer,
+                Player.OPlayer, Player.XPlayer, Player.XPlayer,
+                Player.OPlayer, Player.XPlayer, Player.OPlayer
+            };
+
+            bool expected = true;
+            Player expectedWinner = Player.None;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+
+        [TestMethod]
+        public void IsTerminal_Test12()
+        {
+            Player[] board = new Player[9]
+            {
+                Player.OPlayer, Player.XPlayer, Player.OPlayer,
+                Player.XPlayer, Player.OPlayer, Player.XPlayer,
+                Player.XPlayer, Player.OPlayer, Player.XPlayer
+            };
+
+            bool expected = true;
+            Player expectedWinner = Player.None;
+
+            bool actual = TicTacToeSolver.IsTerminal(board, out Player actualWinner);
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedWinner, actualWinner);
+        }
+    }
+}
---
+++ CSharp/SolverTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SolverTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SolverTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("407a30b8-4916-4559-9b23-1e35eaada4be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
